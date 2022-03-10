a118024f8fe4b91c60a910dfc530098a124876f5
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-10 18:19:42 +0100

    snack 2

--- snack/script.js
+++ snack/script.js
@@ -1,4 +1,12 @@
 //SNACK1
-const str=['pippo', 'PLUTO', 'Paperino'];
-const toLowerCaseStr=str.map(x=>x.toLowerCase());
-const formattedStr=toLowerCaseStr.map(x=>x.charAt(0).toUpperCase()+x.slice(1));
+const str = ['pippo', 'PLUTO', 'Paperino'];
+const toLowerCaseStr = str.map(x => x.toLowerCase());
+const formattedStr = toLowerCaseStr.map(x => x.charAt(0).toUpperCase() + x.slice(1));
+
+//SNACK2
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+const mammiferi = [animals.filter(animal => animal.classe == 'mammiferi')];
